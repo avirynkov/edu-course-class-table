726711f89d1939005e0785985d2a30b85b3e655d
Avi Rynkov <avi@rynkov.eu>
2023-02-06 18:45:30 +0200

    add validation

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import { getRandomNumber, sayHello } from './utils/random.js';
 
 const form = document.querySelector('#form');
 const tbody = document.querySelector('#table-body');
+let id = 124;
 
 
 function addRow(name, age) {
     tbody.innerHTML += `
         <tr>
-          <td>${getRandomNumber(100, 1000)}</td>
+          <td>${increment()}</td>
           <td>${name}</td>
           <td>${age}</td>
         </tr>
@@ -21,16 +22,47 @@ function resetForm() {
 }
 
 
+function validateData(name, age) {
+    if (!name) {
+        alert('Name is required!');
+        return false;
+    }
+
+    if (!age) {
+        alert('Age is required!');
+        return false;
+    }
+
+    if (age < 18) {
+        alert('Age must be >18!');
+        return false;
+    }
+
+    if (age > 120) {
+        alert('Are you serious?');
+        return false;
+    }
+
+    return true;
+}
+
+function increment() {
+    id += 1;
+    return id;
+}
+
+
 function onSubmit(event) {
     event.preventDefault();
     const name = form.elements['name'].value;
     const age = form.elements['age'].value;
 
-    addRow(name, age);
-    resetForm();
+    const isValid = validateData(name, age);
 
-
+    if (isValid) {
+        addRow(name, age);
+        resetForm();
+    }
 }
-
 
 form.addEventListener('submit', onSubmit);
